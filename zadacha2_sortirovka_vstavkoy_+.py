4896935fd28328489f9e329254dd733641d1af6f
Polina-Solovyova <90193243+Polina-Solovyova@users.noreply.github.com>
2021-10-06 21:00:38 +0300

    Update zadacha2_sortirovka_vstavkoy_+.py

--- zadacha2_sortirovka_vstavkoy_+.py
+++ zadacha2_sortirovka_vstavkoy_+.py
@@ -8,13 +8,18 @@ x = open('/Users/Guest/Documents/output.txt', 'w')
        
 def insertion_sort(nums):
     for i in range(1, n):
-        item_to_insert = nums[i]
-        j = i - 1
-        while j >= 0 and nums[j] > item_to_insert:
-            nums[j + 1] = nums[j]
-            j -= 1
-        nums[j + 1] = item_to_insert
-        ind.append(j + 2)
+        if abs(nums[i]) <= 10 ** 9:
+            item_to_insert = nums[i]
+            j = i - 1
+            while j >= 0 and nums[j] > item_to_insert:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = item_to_insert
+            ind.append(j + 2)
+        else:
+            x = open('/Users/Guest/Documents/output.txt', 'w')
+            x.write('ошибка')
+            exit()
         
 if n >= 1 and n <= 10 ** 3:
     insertion_sort(nums)
