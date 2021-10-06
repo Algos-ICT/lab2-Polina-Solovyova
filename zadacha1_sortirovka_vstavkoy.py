5482416b5d63da6a9cc1e86888326b91a29e8cf0
Polina-Solovyova <90193243+Polina-Solovyova@users.noreply.github.com>
2021-10-06 20:44:38 +0300

    Update zadacha1_sortirovka_vstavkoy.py

--- zadacha1_sortirovka_vstavkoy.py
+++ zadacha1_sortirovka_vstavkoy.py
@@ -3,26 +3,28 @@ n = f.readline()
 n = int(n)
 nums = f.readline()
 nums = list(map(int, nums.split()))
+x = open('/Users/Guest/Documents/output.txt', 'w')
        
 def insertion_sort(nums):
-    if n >= 1 and n <= 10 ** 3:
-        for i in range(1, n):
-            item_to_insert = nums[i]
+    for i in range(1, n):
+        item_to_insert = nums[i]
+        if abs(nums[i]) <= 10 ** 9:
             j = i - 1
-            if abs(nums[i]) <= 10 ** 9:
-                while j >= 0 and nums[j] > item_to_insert:
-                    nums[j + 1] = nums[j]
-                    j -= 1
-                nums[j + 1] = item_to_insert
-            else:
-                print('ошибка')
-    else:
-        print('ошибка')
- 
-insertion_sort(nums)
-out = ' '.join(str(e) for e in nums)
-x = open('/Users/Guest/Documents/output.txt', 'w')
-x.write(out)
+            while j >= 0 and nums[j] > item_to_insert:
+                nums[j + 1] = nums[j]
+                j -= 1
+            nums[j + 1] = item_to_insert
+        else:
+            x.write('ошибка')
+            exit()
+            
+if n >= 1 and n <= 10 ** 3:
+    insertion_sort(nums)
+    out = ' '.join(str(e) for e in nums)
+    x.write(out)
+else:
+    x = open('/Users/Guest/Documents/output.txt', 'w')
+    x.write('ошибка')
+    
 f.close()
 x.close()
-
