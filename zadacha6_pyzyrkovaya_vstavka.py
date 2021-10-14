72669d272ae44b60c191608ea4f3a347ce163a6b
Polina-Solovyova <90193243+Polina-Solovyova@users.noreply.github.com>
2021-10-14 14:46:38 +0300

    Update zadacha6_pyzyrkovaya_vstavka.py

--- zadacha6_pyzyrkovaya_vstavka.py
+++ zadacha6_pyzyrkovaya_vstavka.py
@@ -3,24 +3,25 @@ n = f.readline()
 n = int(n)
 nums = f.readline()
 nums = list(map(int, nums.split()))
-       
-def insertion_bubble(A: list):
-    if n >= 1 and n <= 10 ** 3:
-        for j in range(0, n):
-            for i in range(0, n - 1):
-                if abs(nums[i]) <= 10 ** 9:
-                    if A[i] > A[i + 1]:
-                        A[i + 1], A[i] = A[i], A[i + 1]
-                else:
-                    print('ошибка')
-        return A
-    else:
-        print('ошибка')
-        
-insertion_bubble(nums)
-out = ' '.join(str(e) for e in nums)
 x = open('/Users/Guest/Documents/output.txt', 'w')
-x.write(out)
+
+def insertion_bubble(A: list):
+       for j in range(0, n):
+              for i in range(0, n - 1):
+                     if A[i] > A[i + 1]:
+                            A[i + 1], A[i] = A[i], A[i + 1]
+       return A
+
+if n <= 1 and n >= 10 ** 3:
+       x.write('ошибка')
+       for i in nums:
+              if abs(i) >= 10 ** 9:
+                    x.write('ошибка')
+else:
+       insertion_bubble(nums)
+       out = ' '.join(str(e) for e in nums)
+       x.write(out)
+
 f.close()
 x.close()
 
